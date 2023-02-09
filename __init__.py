6cac06497ecfe4f39d06d4972a58f96391943029
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-02-09 19:56:23 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,8 +9,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-        elif extension_id == "mobile":
-            self.extension = MobileExtension(self.bus, self.gui)
 #
 from enum import Enum
 from time import sleep
